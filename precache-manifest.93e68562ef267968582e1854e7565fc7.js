1282473805cba2446ab1fbdff622136e75503451
steven <steven.yjj1995@gmail.com>
2021-04-18 16:17:45 +0800

    deploy

---
+++ precache-manifest.93e68562ef267968582e1854e7565fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2673cb2be3cf1563e11c",
+    "url": "/fafa-Bookkeeping-website/css/app.fdc272a1.css"
+  },
+  {
+    "revision": "7b1cc074f0ffcecc625c5085607c41a9",
+    "url": "/fafa-Bookkeeping-website/index.html"
+  },
+  {
+    "revision": "2673cb2be3cf1563e11c",
+    "url": "/fafa-Bookkeeping-website/js/app.366278cf.js"
+  },
+  {
+    "revision": "7f41f787427b7d8473f4",
+    "url": "/fafa-Bookkeeping-website/js/chunk-vendors.05919558.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/fafa-Bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "642378995f173cd99415cd1ac028d730",
+    "url": "/fafa-Bookkeeping-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fafa-Bookkeeping-website/robots.txt"
+  }
+]);
